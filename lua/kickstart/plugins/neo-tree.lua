a9eec5e084eb02e9e3947c4c7603971ec5dae468
michal-weglarz <morteify@icloud.com>
2025-01-31 17:59:07 +0100

    feat: follow current file in neo-tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -15,6 +15,9 @@ return {
   },
   opts = {
     filesystem = {
+      follow_current_file = {
+        enabled = true,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
