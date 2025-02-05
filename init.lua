ce71f423420b723e813d5b036908d589d67dd587
michal-weglarz <morteify@icloud.com>
2025-02-05 18:08:54 +0100

    feat: display sidebar on the right

--- init.lua
+++ init.lua
@@ -924,7 +924,14 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
-
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    opts = {
+      window = {
+        position = 'right',
+      },
+    },
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
