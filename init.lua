e260b568b812c8d4b5653c13de93f7a406bef504
michal-weglarz <morteify@icloud.com>
2025-04-24 19:40:35 +0200

    show hidden files in neotree

--- init.lua
+++ init.lua
@@ -931,6 +931,11 @@ require('lazy').setup({
       window = {
         position = 'right',
       },
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
     },
   },
   {
