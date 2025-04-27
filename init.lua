cce826981b5696e9f2dafa5d055f2ff37b9e5e06
michal-weglarz <morteify@icloud.com>
2025-04-27 19:53:39 +0200

    chore: import custom plugins

--- init.lua
+++ init.lua
@@ -720,6 +720,7 @@ require('lazy').setup({
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
         typescript = { 'prettierd', 'prettier', stop_after_first = true },
         c = { 'clang-format' },
+        cs = { 'csharpier' },
       },
     },
   },
@@ -975,7 +976,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
